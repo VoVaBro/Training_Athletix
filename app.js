585f0154397c7a9409a7259dc58140c672eaba16
VladimirBrozhik <vbrozhik@gmail.com>
2019-11-06 20:31:15 +0300

    .

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const {PORT} = require ('./sittings/config');
 const app = express();
 
 
+app.get('/', (req, res) => res.send('hello'));
 
 
+app.listen(PORT, () => console.log(`server start on port ${PORT}`));
 
-app.listen(PORT, () => console.log(`server start on portn ${PORT}`));
