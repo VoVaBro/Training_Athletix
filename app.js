36e4929acde8591efc9d03e56923a48c91f673d7
Grigoriy Marchenkov <wowg195@gmail.com>
2019-11-07 01:21:47 +0300

    express.static add

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require ('express');
 const {PORT} = require ('./settings/config');
+const path = require("path");
 
 const app = express();
 
+app.use(express.static(path.join(__dirname,"public")));
 
-app.get('/', (req, res) => res.send('hello'));
+app.get('/', (req, res) =>{
+        res.send('hello');
+    });
 
 
 app.listen(PORT, () => console.log(`server start on port ${PORT}`));
