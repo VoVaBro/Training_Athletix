07f1e271a4484605dfa8dee14b7a0bea11a3ca51
VladimirBrozhik <brozhikvladimir6@gmail.com>
2019-11-07 13:22:41 +0300

    DB connection

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
 const express = require ('express');
-const {PORT} = require ('./settings/config');
+const {PORT, MONGO_URI} = require ('./settings/config');
 const path = require("path");
 const exphbs  = require('express-handlebars');
+const mongoose = require ('mongoose');
+
+
 
 const app = express();
 
-// Handlebars Middleware
+// Handlebars Middleware -> ХУЕНДЛ
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
 
-// Static folder
+// Static folder -> ХУЯТИК
 app.use(express.static(path.join(__dirname,"public")));
 
 app.get('/', (req, res) =>{
         res.render("index",{layout: false});
     });
 
-// Не успел создать роутер
+// Не успел создать роутер -> МНЕ ЖЕСТКО ПОХУЙ
 app.get('/record', (req, res) =>{
     res.render("record",{layout: false});
 });
 
+mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log('DB connected');
+        app.listen(PORT, () => console.log(`server start on port ${PORT}`));
+        }).catch(err => console.log('Error:', err ));
 
-app.listen(PORT, () => console.log(`server start on port ${PORT}`));
 
--- settings/config.js
+++ settings/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGO_URI: '',
+    MONGO_URI: 'mongodb+srv://brozhik_vladimir:ER15v62EJdDwS4jN@cluster0-nudxi.mongodb.net/T_A?retryWrites=true&w=majority',
     PORT: process.env.PORT || 3000,
     jwt: {
         secret: 'nobodycanusethisapp'
