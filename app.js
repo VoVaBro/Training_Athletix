43f16b1694b37c3b3d4b890b089f2aea10d1f3cf
Grigoriy Marchenkov <wowg195@gmail.com>
2019-12-18 19:04:29 +0300

    .

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.engine('hbs', exphbs({
 app.set('view engine', 'hbs');
 
 //Static folders
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname,'public')));
 
 //Sessions
 const sess = {
